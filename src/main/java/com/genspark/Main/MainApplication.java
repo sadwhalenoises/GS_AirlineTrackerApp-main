9d896a7904ccdfc918a88f43183418476abb4188
John Albesa <jnkziaa@gmail.com>
2022-07-23 00:13:17 -0700

    added some UI

---
+++ src/main/java/com/genspark/Main/MainApplication.java
@@ -0,0 +1,33 @@
+package com.genspark.Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Objects;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/View/second.fxml"));
+        System.out.println(getClass().getResource("/CssStyle/backgroundStyle.css"));
+        //Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("View/aviation.fxml")));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+
+    //HELLO JOHN FROM MARTIN
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,14 @@
 module com.example.gs_airlinetrackerappmain {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires validatorfx;
-                requires org.kordamp.bootstrapfx.core;
-            
-    opens com.example.gs_airlinetrackerappmain to javafx.fxml;
-    exports com.example.gs_airlinetrackerappmain;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.genspark.Main to javafx.fxml;
+    exports com.genspark.Main;
+    exports Controller;
+    opens Controller to javafx.fxml;
 }
